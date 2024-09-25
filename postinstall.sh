3063120339b4886278a2f8305a01fbc717a10361
xnngee <105088936+xnngee@users.noreply.github.com>
2024-09-25 14:54:39 +0300

    v3

--- postinstall.sh
+++ postinstall.sh
@@ -24,12 +24,12 @@ EOF
 
 manage_apps() {
     sudo apt remove -y fly-scan fly-admin-iso fly-jobviewer fly-admin-usbip fly-admin-multiseat k3b recoll guvcview
-    sudo apt install -y fish zenity systemd-timesyncd fly-dm-rdp xrdp vino fonts-inter  
+    sudo apt install -y fish zenity systemd-timesyncd fly-dm-rdp xrdp vino fonts-inter
     sudo apt autoremove -y
 }
 
 configure_os() {
-sudo tee ~/.config/fish/config.fish &>/dev/null <<'EOF'
+tee ~/.config/fish/config.fish &>/dev/null <<EOF
 if status is-interactive
     ### Functions
     function fishhelp
@@ -58,26 +58,25 @@ if status is-interactive
 
     function fish_greeting
         ### Minimal system info from screenfetch sources (fishfetch)
-        echo "$(set_color yellow)Logged as:  $(whoami)$(set_color normal)@$(set_color yellow)$(hostname)$(set_color normal)"
-        echo "$(set_color yellow)OS:$(set_color normal)         $(grep '^NAME=' /etc/os-release | cut -d '"' -f 2)"
-        # echo "OS: $(lsb_release -si) $(lsb_release -sr) ($(lsb_release -sc))"
-        echo "$(set_color yellow)Kernel:$(set_color normal)     $(uname -m) $(uname -sr)"
-        echo "$(set_color yellow)Shell:$(set_color normal)      $(echo $SHELL)"
-        echo "$(set_color yellow)CPU:$(set_color normal)       $(awk -F':' '/^model name/ {split($2, A, " @"); print A[1]; exit}' /proc/cpuinfo) Cores: $(grep -c '^cpu core' /proc/cpuinfo)"
-        set mem "$(free -b | awk -F ':' 'NR==2{print $2}' | awk '{print $1"-"$6}')"
-        set memsplit (string split "-" -- $mem)
+        echo "\$(set_color yellow)Logged as:  \$(whoami)\$(set_color normal)@\$(set_color yellow)\$(hostname)\$(set_color normal)"
+        echo "\$(set_color yellow)OS:\$(set_color normal)         \$(grep '^NAME=' /etc/os-release | cut -d '"' -f 2)"
+        # echo "OS: \$(lsb_release -si) \$(lsb_release -sr) (\$(lsb_release -sc))"
+        echo "\$(set_color yellow)Kernel:\$(set_color normal)     \$(uname -m) \$(uname -sr)"
+        echo "\$(set_color yellow)CPU:\$(set_color normal)       \$(awk -F':' '/^model name/ {split(\$2, A, " @"); print A[1]; exit}' /proc/cpuinfo) Cores: \$(grep -c '^cpu core' /proc/cpuinfo)"
+        set mem "\$(free -b | awk -F ':' 'NR==2{print \$2}' | awk '{print \$1"-"\$6}')"
+        set memsplit (string split "-" -- \$mem)
         # https://stackoverflow.com/questions/34188178/how-to-extract-substring-in-fish-shell
-        # set memsplitused (string split "-" -- $mem)[1]
-        # set memsplittotal (string split "-" -- $mem)[2]
-		set usedmem "$(math -s 1 $(math $mem) / 1024 / 1024)"
-		set totalmem "$(math -s 1 $memsplit[1] / 1024 / 1024)"
-        echo "$(set_color yellow)RAM:$(set_color normal)        $usedmem MiB / $totalmem MiB"
-		echo "$(set_color yellow)IP:$(set_color normal)         $(hostname -I)'('$(curl -s 2ip.io)')'"
+        # set memsplitused (string split "-" -- \$mem)[1]
+        # set memsplittotal (string split "-" -- \$mem)[2]
+		set usedmem "\$(math -s 1 \$(math \$mem) / 1024 / 1024)"
+		set totalmem "\$(math -s 1 \$memsplit[1] / 1024 / 1024)"
+        echo "\$(set_color yellow)RAM:\$(set_color normal)        \$usedmem MiB / \$totalmem MiB"
+		echo "\$(set_color yellow)IP:\$(set_color normal)         \$(hostname -I)'('\$(curl -s 2ip.io)')'"
         echo "Enter 'fishhelp' or 'fh' for more info"
     end
 
     function last_history_item
-        echo $history[1]
+        echo \$history[1]
     end
 
     ### Abbreviations
@@ -117,6 +116,8 @@ configure_de() {
     fly-admin-theme apply-color-scheme /usr/share/color-schemes/AstraProximaAdmin.colors
 
     sudo fly-wmfunc FLYWM_UPDATE_VAL TaskbarHeight 38
+    sudo fly-wmfunc FLYWM_UPDATE_VAL WallPaper "/usr/share/wallpapers/avi.jpg"
+    sudo fly-wmfunc FLYWM_UPDATE_VAL LogoPixmap ""
 
     sudo fly-wmfunc FLYWM_UPDATE_VAL LockerOnDPMS false
     sudo fly-wmfunc FLYWM_UPDATE_VAL LockerOnLid false
@@ -129,12 +130,12 @@ configure_de() {
     sudo fly-wmfunc FLYWM_UPDATE_VAL DialogFont "Inter Display-10:normal"
     sudo fly-wmfunc FLYWM_UPDATE_VAL IconFont "Inter Display-9:normal"
     sudo fly-wmfunc FLYWM_UPDATE_VAL StartMenuFont "Inter Display-10:normal"
-    sudo fly-wmfunc FLYWM_UPDATE_VAL TaskbarBoldFont "Inter Display-9:bold"
+    sudo fly-wmfunc FLYWM_UPDATE_VAL TaskbarBoldFont "Inter Display-10:bold"
     sudo fly-wmfunc FLYWM_UPDATE_VAL TaskbarClockFont "Inter Display-12:normal"
     sudo fly-wmfunc FLYWM_UPDATE_VAL TaskbarDateFont "Inter Display-9:normal"
-    sudo fly-wmfunc FLYWM_UPDATE_VAL TaskbarFont "Inter Display-9:normal"
+    sudo fly-wmfunc FLYWM_UPDATE_VAL TaskbarFont "Inter Display-10:normal"
     sudo fly-wmfunc FLYWM_UPDATE_VAL TaskbarLangFont "Inter Display-10:normal"
-    sudo fly-wmfunc FLYWM_UPDATE_VAL TitleFont "Inter Display-9:normal"
+    sudo fly-wmfunc FLYWM_UPDATE_VAL TitleFont "Inter Display-10:normal"
     sudo fly-wmfunc FLYWM_UPDATE_VAL TooltipFont "Inter Display-10:normal"
     sudo fly-wmfunc FLYWM_UPDATE_VAL CascadeMenuFont "Inter Display-10:normal"
     sudo fly-wmfunc FLYWM_UPDATE_VAL LockerEnterFont "Inter Display-13:normal"
@@ -145,8 +146,6 @@ configure_de() {
     sudo fly-wmfunc FLYWM_UPDATE_VAL LockerUsernameFont "Inter Display-13:normal"
     sudo fly-wmfunc FLYWM_UPDATE_VAL LockerWeekDayFont "Inter Display-11:normal"
     sudo fly-wmfunc FLYWM_UPDATE_VAL LockerWelcomeFont "Inter Display-14:normal"
-
-    sudo fly-wmfunc FLYWM_UPDATE_VAL WallPaper "/usr/share/wallpapers/avi.jpg"
     sudo fly-wmfunc FLYWM_NUMLOCK_ON
 
     # zenity --info --width=420 --title="AstraLinux 1.8 PostInstall" --text="Автоматизировать часть настроек нет возожности, требуется ваше вмешательство!\n\nПеред вами открылись 'Параметры системы' со страницей 'Вход в систему' (Настройка графического входа)\nАктивируйте следующие пункты:\n- Вкладка 'Основное':\n  Состояние NumLock\n- Вкладка 'Тема':\n  Очистить строки в 'Выбрать обои' и 'Логотип' (его отключите)\n- Вкладка 'Дополнительно' (опционально):\n   Включите 'Разрешить автоматический вход в систему'\n  Выберите пользователя\n  Укажите пользователя в 'Автоматически выбирать пользователя'\n  Включите 'Переместить фокус на поле ввода пароля'\nНажмите 'Применить' и закройте окно" &
@@ -177,23 +176,81 @@ configure_de() {
 }
 
 cds_setup() {
-# start script
-cat << EOF >> /home/cds/start-cds.sh
+    sudo apt install -y git chromium
+    zenity --info --widht=300 --title="Установите 2 расширения (Cors Unblock, Browsec) для браузера и включите их для корректной работы CDS." &
+    chromium https://chromewebstore.google.com/detail/cors-unblock/lfhmikememgdcahcdlaciloancbhjino
+    chromium https://chromewebstore.google.com/detail/%D0%B2%D0%BF%D0%BD-%D0%B4%D0%BB%D1%8F-youtube-browsec-v/omghfjlpggmjjaagoclmmobgdodcjboh
+    rm -rf ~/cds-monitoring-board-kat
+    mkdir ~/cds-monitoring-board-kat
+    git clone https://github.com/xenongee/cds-monitoring-board-kat.git ~/cds-monitoring-board-kat/
+    sed -i '10s|https://some.url/aviatech/index.php|https://map.cdsvyatka.com/avia/|' ~/cds-monitoring-board-kat/assets/app.js
+
+    # start script
+tee ~/cds-monitoring-board-kat/cds-start.sh &>/dev/null <<EOF
 #!/bin/bash
-xset -dpms
-xset s off
-# cds_url="https://map.cdsvyatka.com/avia/index3.php"
-cds_url="0x0.st"
-while true; do
-    rm -rf ~/.{config,cache}/chromium/
-    chromium-browser --kiosk --no-first-run --disable-translate \$cds_url
-    sleep 1s
+rm -rf ~/.{config,cache}/chromium/
+cds_url="file:///home/$USER/cds-monitoring-board-kat/index.html"
+x=1
+while [ \$x -le 5 ]
+do
+    #--no-first-run
+    chromium --kiosk --disable-translate \$cds_url
+    sleep 5s
+  x=\$(( \$x + 1 ))
 done
 EOF
-chown cds:cds /home/cds/start-cds.sh
-chmod 755 /home/cds/start-cds.sh
-chmod +x /home/cds/start-cds.sh
+    chown "$USER":"$USER" ~/cds-monitoring-board-kat/cds-start.sh
+    chmod 755 ~/cds-monitoring-board-kat/cds-start.sh
+    chmod +x ~/cds-monitoring-board-kat/cds-start.sh
 
+    #systemd service
+sudo tee /etc/systemd/system/cds-monitoring.service &>/dev/null <<EOF
+[Unit]
+Description=Run CDS.Vyatka monitoring page
+PartOf=graphical.target
+After=network.target
+[Service]
+User=$USER
+Group=$USER
+Environment="DISPLAY=:0"
+Environment="XAUTHORITY=~/.Xauthority"
+ExecStart=/home/$USER/cds-monitoring-board-kat/cds-start.sh
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo tee /etc/systemd/system/cds-monitoring-restart.service &>/dev/null <<EOF
+[Unit]
+Description=Restarts 'cds-monitoring.service' every night
+PartOf=graphical.target
+After=network.target
+[Service]
+ExecStart=/usr/bin/systemctl restart cds-monitoring.service
+Restart=on-failure
+RestartSec=30s
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo tee /etc/systemd/system/cds-monitoring-restart.timer &>/dev/null <<EOF
+[Unit]
+Description=Restarts 'cds-monitoring.service' every night
+PartOf=graphical.target
+After=network.target
+[Timer]
+OnCalendar=*-*-* 00:00
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    sleep 3
+
+    sudo systemctl enable cds-monitoring.service 
+    sudo systemctl enable cds-monitoring-restart.timer 
+    sudo systemctl start cds-monitoring.service 
+    sudo systemctl start cds-monitoring-restart.timer 
+    sudo systemctl daemon-reload
 }
 
 logout() {
@@ -226,7 +283,7 @@ while true; do
     echo ""
     echo "> AstraLinux 1.8 PostInstall"
     echo ""
-    echo "1. Auto PostInstall (by 2 to 6 functions)"
+    echo "1. Auto PostInstall (from 2 to 7 function)"
     echo "2.   Set Time via NTP"
     echo "3.   Enable AstraLinux Repositories"
     echo "4.   Manage Apps - remove unneeded packages and install RDP, VNC and some bits"
